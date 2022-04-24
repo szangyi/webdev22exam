4ae01a1c587b1cad78ea48a03fbb5dcab89314f9
Alexandra Mészáros <alexandra.mszrs@gmail.com>
2022-04-24 13:46:00 +0200

    index logged out rewrote to pythonanywhere

--- home_get_loggedout.py
+++ home_get_loggedout.py
@@ -12,23 +12,44 @@ import pymysql
 @get("/index_loggedout")
 @view("index_loggedout")
 def _():
+    ### DEFINE THE VARIABLES ###
+    response.set_header("Cache-Control", "no-cache, no-store, must-revalidate")
+    user_session_id = request.get_cookie("uuid4")
+    print("--usersession cookie")
+    print(user_session_id)
+    print("*******not logged in")
 
     try:
-        ### DEFINE THE VARIABLES ###
-        response.set_header("Cache-Control", "no-cache, no-store, must-revalidate")
-        user_session_id = request.get_cookie("uuid4")
-        print("--usersession cookie")
-        print(user_session_id)
-        print("*******not logged in")
-        
-        ### VALIDATE ###
+        print("production mode")
+        import production
+        db_config = {
+        "host":"szangyi.mysql.eu.pythonanywhere-services.com", 
+        "user":"szangyi", 
+        "password":"sedHuq-piwdyh-xergy9", 
+        "database":"szangyi$twitter", 
+        "cursorclass":pymysql.cursors.DictCursor
+        }
 
+    except Exception as ex:
+        print("development mode")
+        print(ex)
+        db_config = {
+        "host":"localhost", 
+        "port":8889,
+        "user":"root", 
+        "password":"root", 
+        "database":"twitter", 
+        "cursorclass":pymysql.cursors.DictCursor
+        }
 
+    try:
         ### CONNECT TO DB AND EXECUTE ###
+        db = pymysql.connect(**db_config)
+        cur = db.cursor()
 
         # tweets + user info + tweet user image
-        db = pymysql.connect(host="localhost", port=8889,user="root",password="root", database="twitter", cursorclass=pymysql.cursors.DictCursor)
-        cur = db.cursor() 
+        # db = pymysql.connect(host="localhost", port=8889,user="root",password="root", database="twitter", cursorclass=pymysql.cursors.DictCursor)
+        # cur = db.cursor() 
         sql_tweets = """SELECT * 
         FROM tweets 
         JOIN users
@@ -39,11 +60,7 @@ def _():
         """
         cur.execute(sql_tweets)
         tweets = cur.fetchall() 
-        
-        
-        
-        
-        
+    
         db.commit()
 
         ### RETURN ###
@@ -52,7 +69,7 @@ def _():
             tabs=g.TABS_LOGGEDOUT,
             people=g.PEOPLE,
             trends=g.TRENDS
-                )
+            )
     except Exception as ex:
         print(ex)
     finally:

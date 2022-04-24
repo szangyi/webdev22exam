6ca7a3d0992171f9199cf952108234101fb8e247
Alexandra Mészáros <alexandra.mszrs@gmail.com>
2022-04-24 11:15:16 +0200

    setup.py configured

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from bottle import request
+from bottle import request, default_app
 import git
 
-@app.route('/', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('path/to/git_repo')
-            origin = repo.remotes.origin
-origin.pull()
-return 'Updated PythonAnywhere successfully'
+application = default_app()
+
+application.route('/', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
+        origin.pull()
+    return 'Updated PythonAnywhere successfully'
